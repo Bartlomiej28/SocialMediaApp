e8caf7e78786f2636120eda729dba5fb15259ce3
Bartek <bartlomiej.boczylo@gmail.com>
2024-06-07 23:11:59 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   
   return (
+    <>
     <Routes>
         <Route path="/*" element={<HomePage />} />
         <Route path="/" element={<LoginPage/>} />
     </Routes>
+    <SpeedInsights/>
+    </>
+    
   );
 }
 
